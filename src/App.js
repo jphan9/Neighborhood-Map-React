e1cfda17f6183b58f958b039be157dcf1735d779
Jimmy Phan <jphan9@gmail.com>
2018-10-02 22:32:59 -0700

    feat: Added Google maps onto the project.

--- src/App.js
+++ src/App.js
@@ -3,27 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.loadMap();
+  }
+
+  loadMap= () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDxN2DFeH5_jkVStwLZHfXak8jdW9kjCyY&callback=initMap");
+    window.initMap = this.initMap;
+  }
+
+  initMap = () => {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-            This is so cool!!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id="map"></div>
+      </main>
     );
   }
+}
+
+function loadScript(url) {
+  var tag = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+
+  script.onerror = function() {
+    document.write("Google Maps cannot be loaded.");
+  }
+
+  tag.parentNode.insertBefore(script,tag);
 }
 
 export default App;
