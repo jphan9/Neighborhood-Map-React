f5ff441508362f43420580eb7e2d3c2075f937db
Jimmy Phan <jphan9@gmail.com>
2018-10-03 22:18:41 -0700

    feat: Added map component file and added markers to the map.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Map from './Map';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    markers: [
+      {
+        name: 'Staples Center',
+        lat: 34.0430058,
+        lng: -118.2695484
+      },
+      {
+        name: 'Los Angeles Memorial Colessium',
+        lat: 34.0140526,
+        lng: -118.2900641
+      },
+      {
+        name: 'Rose Bowl Stadium',
+        lat: 34.1613284,
+        lng: -118.1698349
+      },
+      {
+        name: 'Angel Stadium of Anaheim',
+        lat: 33.800308,
+        lng: -117.8849208
+      },
+      {
+        name: 'Honda Center',
+        lat: 33.8078476,
+        lng: -117.8786574
+      },
+      {
+        name: 'Dodger Stadium',
+        lat: 34.073851,
+        lng: -118.242147
+      },
+    ]
+  }
 
   componentDidMount() {
     this.loadMap();
@@ -15,15 +51,23 @@ class App extends Component {
 
   initMap = () => {
     var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: {lat: 34.0517416, lng: -118.3911628},
+      zoom: 10
     });
+
+    this.state.markers.forEach(markerLocation => {
+      var marker = new window.google.maps.Marker({
+        position: {lat:markerLocation.lat, lng: markerLocation.lng},
+        map: map,
+        title: markerLocation.name
+      });
+    })
   }
 
   render() {
     return (
       <main>
-        <div id="map"></div>
+        <Map></Map>
       </main>
     );
   }
---
+++ src/Map.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Map extends Component {
+  render() {
+    return (
+      <div id="map_container">
+        <div id="map"></div>
+      </div>
+    )
+  }
+}
+
+export default Map;
